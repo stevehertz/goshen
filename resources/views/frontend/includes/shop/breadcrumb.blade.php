cc5c6e9dc9efe70c7deffe2c886d4ff1bf731209
stevehertz <stevekamahertz@gmail.com>
2024-10-27 16:19:27 +0300

    Shop breadcrumb

---
+++ resources/views/frontend/includes/shop/breadcrumb.blade.php
@@ -0,0 +1,15 @@
+<section class="breadcrumb-section set-bg" data-setbg="{{ asset('img/shop/breadcrumb.jpg') }}">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="breadcrumb__text">
+                    <h2>Contact Us</h2>
+                    <div class="breadcrumb__option">
+                        <a href="{{ route('shop') }}">Home</a>
+                        <span>Contact Us</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
